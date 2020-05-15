626bd05dd0000ed73be2174ed2df949d66b26618
Bill <bill2019@foxmail.com>
2020-05-15 13:56:02 +0800

    fixed grammar err

--- app.js
+++ app.js
@@ -3,19 +3,19 @@
 const assert = require('assert');
 const mongoose = require('mongoose');
 
-let count = 0;
+// let count = 0;
 
 module.exports = app => {
-    app.addSingleton('mongoose', createOneClient);
+  app.addSingleton('mongoose', createOneClient);
 };
 
-function createOneClient(config, app) {
+async function createOneClient(config, app) {
 
-    assert(config.url && config.options,
-        `[egg-typegoose-js] 'url: ${config.url}', 'options: ${config.options}' are required on config`)
-    const connection = await mongoose.connect(config.url, config.options);
-    //   app.beforeStart(function* () {
-    //   });
+  assert(config.url && config.options,
+    `[egg-typegoose-js] 'url: ${config.url}', 'options: ${config.options}' are required on config`);
+  const connection = await mongoose.connect(config.url, config.options);
+  app.beforeStart(function* () {
+  });
 
-    return connection;
+  return connection;
 }
