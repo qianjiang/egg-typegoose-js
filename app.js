fcc6a0769c21b3efd1e673129027c19b0f479cb5
Bill <bill2019@foxmail.com>
2020-05-15 14:05:31 +0800

    fixed lint

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const mongoose = require('mongoose');
 
 // let count = 0;
 
-module.exports = app => {
+module.exports = async app => {
   app.addSingleton('mongoose', createOneClient);
 };
 
-async function createOneClient(config, app) {
+async function createOneClient(config/* , app */) {
 
   assert(config.url && config.options,
     `[egg-typegoose-js] 'url: ${config.url}', 'options: ${config.options}' are required on config`);
   const connection = await mongoose.connect(config.url, config.options);
-  app.beforeStart(function* () {
-  });
+  // app.beforeStart(function* () {
+  // });
 
   return connection;
 }
